0fce3bc26f254807fbb91b824faa9d7da7c80874
jahanzaibAns <jahanzebansari503@gmail.com>
2025-07-31 00:18:18 +0500

    car details half

---
+++ car-details.php
@@ -0,0 +1,257 @@
+<?php $title = "Affordable Car Rental Dubai, UAE | Best Deals on Rental Cars";
+include 'include/header.php' ?>
+
+<section class="page_banner our_fleet">
+    <div class="container">
+        <h1>Car Details</h1>
+    </div>
+</section>
+
+<section class="car_details_sec">
+    <div class="container">
+        <div class="car_name_share">
+            <div class="row">
+                <div class="col-md-8">
+                    <h2 class="secHeading">
+                        Hyundai Elantra 2024
+                    </h2>
+                    <p>
+                        Dubai Rental: 5 Seater Sedan, White Color, Sleek Design, Advanced Safety Features, Perfect For Daily Commutes
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <div class="btn_flex card_car_btns">
+                        <a href="javascript:void(0);" class="btn call_btn">
+                            <i class="fa-solid fa-phone-volume"></i> Call
+                        </a>
+                        <a href="javascript:void(0);" class="btn wa_btn">
+                            <i class="fa-brands fa-whatsapp"></i> Whatsapp
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="car_gallery_section">
+            <div class="car_gallery_grid">
+                <div class="car_gallery_left">
+                    <a href="https://static.oneclickdrive.com/uploads/cars/Hyundai-Elantra-2024_31570_32712273586-7_small.jpg?ve=3.2" data-fancybox="gallery">
+                        <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai-Elantra-2024_31570_32712273586-7_small.jpg?ve=3.2" alt="Car Interior 1" />
+                    </a>
+                    <a href="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_2561568697-1_small.jpg?ve=3.2" data-fancybox="gallery">
+                        <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_2561568697-1_small.jpg?ve=3.2" alt="Car Dashboard" />
+                    </a>
+                </div>
+                <div class="car_gallery_right">
+                    <a href="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25615673675-1_small.jpg?ve=3.2" data-fancybox="gallery">
+                        <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25615673675-1_small.jpg?ve=3.2" alt="Car Rear" />
+                    </a>
+                    <a href="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25616242918-6_small.jpg?ve=3.2" data-fancybox="gallery">
+                        <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25616242918-6_small.jpg?ve=3.2" alt="Car Front" />
+                    </a>
+                    <a href="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25615631643-1_small.jpg?ve=3.2" data-fancybox="gallery" class="view-all-wrapper">
+                        <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25615631643-1_small.jpg?ve=3.2" alt="Car Side" />
+                        <div class="view-all-overlay">
+                        <span><i class="fa-solid fa-images me-2"></i> View All Photos</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="car_details_specs">
+                    <div class="car_details_head">
+                        <h3>
+                            Hyundai Elantra 2024
+                        </h3>
+                        <ul class="card_car_model">
+                            <li title="The model year of the car">
+                                <span>
+                                    <i class="fa-solid fa-car"></i> 2024
+                                </span>
+                            </li>
+                            <li title="Built for Gulf roads, climate, and standards.">
+                                <span>
+                                    <i class="fa-solid fa-gear"></i> GCC
+                                </span>
+                            </li>
+                            <li title="Car Type">
+                                <span>
+                                    Sedan
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-7">
+                        <h5 class="car_details_heading">
+                           <span> Documents Requirements</span>
+                        </h5>
+                        <h6 class="car_details_sub_heading">
+                            Residents and UAE Nationals
+                        </h6>
+                        <ul class="card_car_features">
+                            <li>
+                                Copy of Driving License & Resident ID
+                            </li>
+                            <li>
+                                Copy of Resident Visa
+                            </li>
+                            <li>
+                                Passport Copy (Only for Residents)
+                            </li>
+                            <li>
+                                Free Service and Maintenance
+                            </li>
+                        </ul>
+                        <h6 class="car_details_sub_heading">
+                            Foreign Visitors
+                        </h6>
+                        <ul class="card_car_features">
+                            <li>
+                                Original Passport or Copy
+                            </li>
+                            <li>
+                                Original Visa or Copy
+                            </li>
+                            <li>
+                                IDP & License Issued from Home Country
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div class="col-md-5">
+                        <div class="card_car_pricing">
+                            <div class="card_pricing">
+                                <span class="original_price">
+                                    <span>AED 150</span>
+                                </span>
+                                <h5 class="dicounted_price">
+                                    AED 120 <span>/ Day</span>
+                                </h5>
+                                <span class="kilometers">
+                                    <i class="fa-solid fa-road me-2"></i> 250 km
+                                </span>
+                            </div>
+                            <div class="card_pricing">
+                                <span class="original_price">
+                                    <span>
+                                        AED 2700
+                                    </span>
+                                </span>
+                                <h5 class="dicounted_price">
+                                    AED 2000 <span>/ Month</span>
+                                </h5>
+                                <span class="kilometers">
+                                    <i class="fa-solid fa-road me-2"></i> 4,500 km
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <h5 class="car_details_heading my-4">
+                           <span>Car Overview</span>
+                        </h5>
+                        <div class="car_deails_overview">
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-car-side"></i>
+                                    Body Type
+                                </span>
+                                <span class="item_span_bold">Sedan</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    Payment Modes
+                                </span>
+                                <span class="item_span_bold">Cash, Credit Card & more</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-car-side"></i>
+                                    Salik / Toll Charges
+                                </span>
+                                <span class="item_span_bold">AED 5</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    Make
+                                </span>
+                                <span class="item_span_bold">Hyundai</span>
+                            </div>
+
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-car-side"></i>
+                                    Model
+                                </span>
+                                <span class="item_span_bold">Elantra</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    Gearbox
+                                </span>
+                                <span class="item_span_bold">Auto</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-car-side"></i>
+                                    Engine Capacity
+                                </span>
+                                <span class="item_span_bold">1.7 Ltrs</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    Seating Capacity
+                                </span>
+                                <span class="item_span_bold">5 passengers</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    No. of Doors
+                                </span>
+                                <span class="item_span_bold">4</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    Fits No. of Bags
+                                </span>
+                                <span class="item_span_bold">2</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-gas-pump"></i>
+                                    Fuel Type
+                                </span>
+                                <span class="item_span_bold">Petrol</span>
+                            </div>
+                            <div class="car_overview_item">
+                                <span>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    Exterior / Interior Color
+                                </span>
+                                <span class="item_span_bold">White / Black</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include 'include/footer.php' ?>
--- our-fleet.php
+++ our-fleet.php
@@ -11,7 +11,7 @@ include 'include/header.php' ?>
         <h2 class="secHeading">
             Rent a Car Range On Drift Car Rental
         </h2>
-        <div class="flex_filter">
+        <div class="flex_filter mb-4">
             <select name="price_filter" id="price_filter" class="form-select">
                 <option value="">--Sort By Price--</option>
                 <option value="high to low daily">Daily (high to low)</option>
@@ -21,14 +21,143 @@ include 'include/header.php' ?>
                 <option value="high to low monthly">Monthly (high to low)</option>
                 <option value="low to high monthly">Monthly (low to high)</option>
             </select>
-            <button class="btn themeBtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Filters</button>
+            <button class="btn themeBtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                aria-controls="offcanvasRight"><i class="fa-solid fa-sliders me-3"></i> Filters</button>
         </div>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+
+        <div class="car_card_gallery">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card_images_container">
+                        <div class="card_images_wrapper">
+                            <div class="card_images_box">
+                                <a href="car-details.php" class="card_images_link" data-testid="Gallery.Link.1" aria-label="Rent a Hyundai in Dubai"></a>
+                                <div class="image_area">
+                                    <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai-Elantra-2024_31570_32712273586-7_small.jpg" height="260" width="320" alt="Rent Hyundai Elantra 2024 in Dubai" title="Rent Hyundai Elantra 2024 in Dubai">
+                                </div>
+                            </div>
+                            <div class="card_images_box">
+                                <a href="car-details.php" class="card_images_link" data-testid="Gallery.Link.2" aria-label="Rent a Hyundai in Dubai"></a>
+                                <div class="image_area">
+                                    <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_2561568697-1_small.jpg" height="260" width="320" alt="Rent Hyundai Elantra 2024 in Dubai" title="Rent Hyundai Elantra 2024 in Dubai">
+                                </div>
+                            </div>
+                            <div class="card_images_box">
+                                <a href="car-details.php" class="card_images_link" data-testid="Gallery.Link.3" aria-label="Rent a Hyundai in Dubai"></a>
+                                <div class="image_area">
+                                    <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25615673675-1_small.jpg" height="260" width="320" alt="Rent Hyundai Elantra 2024 in Dubai" title="Rent Hyundai Elantra 2024 in Dubai">
+                                </div>
+                            </div>
+                            <div class="card_images_box">
+                                <a href="car-details.php" class="card_images_link" data-testid="Gallery.Link.4" aria-label="Rent a Hyundai in Dubai"></a>
+                                <div class="image_area">
+                                    <img src="https://static.oneclickdrive.com/uploads/cars/Hyundai_Elantra_2024_31570_31570_25616242918-6_small.jpg" height="260" width="320" alt="Rent Hyundai Elantra 2024 in Dubai" title="Rent Hyundai Elantra 2024 in Dubai">
+                                    <div class="find_out_more">
+                                        <div class="d-block">
+                                            <span class="d-block mb-2">
+                                                Like what you see?
+                                            </span>
+                                            <div class="btn findMoreBtn">Find out more </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="car_card_content">
+                        <a href="car-details.php">
+                            <h3 class="card_car_title">
+                                <span>Hyundai Elantra 2024</span>
+                                <!-- Dubai Rental: 5 Seater Sedan, White Color, Sleek Design, Advanced Safety Features, Perfect For Daily Commutes -->
+                            </h3>
+                        </a>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <ul class="card_car_model">
+                                    <li title="The model year of the car">
+                                        <span>
+                                            <i class="fa-solid fa-car"></i> 2024
+                                        </span>
+                                    </li>
+                                    <li title="Built for Gulf roads, climate, and standards.">
+                                        <span>
+                                            <i class="fa-solid fa-gear"></i> GCC
+                                        </span>
+                                    </li>
+                                    <li title="Car Type">
+                                        <span>
+                                            Sedan
+                                        </span>
+                                    </li>
+                                </ul>
+                                <ul class="card_car_features">
+                                    <li>
+                                        Free Delivery
+                                    </li>
+                                    <li>
+                                        Easy Booking Process
+                                    </li>
+                                    <li>
+                                        Contract terms apply
+                                    </li>
+                                    <li>
+                                        Free Service and Maintenance
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="card_car_pricing">
+                                    <div class="card_pricing">
+                                        <span class="original_price">
+                                          <span>AED 150</span>
+                                        </span>
+                                        <h5 class="dicounted_price">
+                                            AED 120 <span>/ Day</span>
+                                        </h5>
+                                        <span class="kilometers">
+                                            <i class="fa-solid fa-road me-2"></i> 250 km
+                                        </span>
+                                    </div>
+                                    <div class="card_pricing">
+                                        <span class="original_price">
+                                            <span>
+                                                AED 2700
+                                            </span>
+                                        </span>
+                                        <h5 class="dicounted_price">
+                                            AED 2000 <span>/ Month</span>
+                                        </h5>
+                                        <span class="kilometers">
+                                            <i class="fa-solid fa-road me-2"></i> 4,500 km
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="btn_flex card_car_btns">
+                            <a href="javascript:void(0);" class="btn call_btn">
+                                <i class="fa-solid fa-phone-volume"></i> Call
+                            </a>
+                            <a href="javascript:void(0);" class="btn wa_btn">
+                                <i class="fa-brands fa-whatsapp"></i> Whatsapp
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="offcanvas offcanvas-end filters_offcanvas" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
             <div class="offcanvas-header">
                 <h5 id="offcanvasRightLabel">Filters</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body">
+            <div class="offcanvas-body" data-lenis-prevent-wheel data-lenis-prevent-touch>
                 <div class="car_type filter_body">
                     <h4>
                         Vehicle Type
@@ -69,7 +198,7 @@ include 'include/header.php' ?>
                             Sports
                         </label>
                     </div>
-                    
+
                 </div>
                 <div class="filter_body">
                     <h4>
@@ -124,14 +253,16 @@ include 'include/header.php' ?>
                         <div class="price_range">
                             <label for="max_day_budget">Max Day Budget</label>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" aria-label="Max Day Budget" name="max_day_budget" aria-describedby="max_day_budget">
+                                <input type="number" class="form-control" aria-label="Max Day Budget"
+                                    name="max_day_budget" aria-describedby="max_day_budget">
                                 <span class="input-group-text" id="max_day_budget">AED</span>
                             </div>
                         </div>
                         <div class="price_range">
                             <label for="max_monthly_budget">Max Monthly Budget</label>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" aria-label="Max Monthly Budget" name="max_monthly_budget" aria-describedby="max_monthly_budget">
+                                <input type="number" class="form-control" aria-label="Max Monthly Budget"
+                                    name="max_monthly_budget" aria-describedby="max_monthly_budget">
                                 <span class="input-group-text" id="max_monthly_budget">AED</span>
                             </div>
                         </div>
@@ -145,5 +276,8 @@ include 'include/header.php' ?>
         </div>
     </div>
 </section>
+
+
+
 
 <?php include 'include/footer.php' ?>
